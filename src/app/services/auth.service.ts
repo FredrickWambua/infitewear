e1bd90fcc534e405bd08adf1f3130702852021a0
FredrickWambua <fredrickswambua254@gmail.com>
2022-06-22 00:05:48 +0300

    sets auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, throwError, map } from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
 
 
 @Injectable({
@@ -11,6 +12,7 @@ import { catchError, Observable, throwError } from 'rxjs';
 export class AuthService {
    AUTH_API: string = 'http://localhost:8080/api/users';
    httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+   currentUser = {};
 
   
   constructor(private http:HttpClient, private router:Router ) { }
@@ -22,14 +24,37 @@ export class AuthService {
 
   }
 
-  loginUser(user:User): Observable<any>{
-    let API_URL = `${this.AUTH_API}/login`;
-    return this.http.post(API_URL, user)
-    .pipe(catchError(this.handleError));
+  loginUser(user:User){
+    return this.http.post(`${this.AUTH_API}/login`, user)
+    .subscribe((data:any)=>{
+      localStorage.setItem('access_token', data.token);
+      this.getUser(data.id).subscribe((data)=>{
+        this.currentUser = data;
+      })
+    })
+  }
+
+  getToken(){
+    return localStorage.getItem('access_token');
+  }
+
+  get isLoggedIn(): boolean {
+    let authToken = localStorage.getItem('access_token');
+    return authToken !==null ? true : false
   }
 
   logoutUser(user:User){
     localStorage.clear()
+  }
+
+  getUser(id:any):Observable<any>{
+    let API_URL = `${this.AUTH_API}/getUser/${id}`;
+    return this.http.get(API_URL, {headers:this.httpHeaders}).pipe(
+      map((data)=>{
+        return data || {}
+      }),
+      catchError(this.handleError)
+    )
   }
 
   handleError(error:HttpErrorResponse){
